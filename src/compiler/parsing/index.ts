a1e6277647e60a00529e592082f65f980a5a35db
troppydash <troppydash.developer@gmail.com>
2021-05-18 20:48:39 +1200

    Reversed the @ variable assignment

--- src/compiler/parsing/index.ts
+++ src/compiler/parsing/index.ts
@@ -850,9 +850,9 @@ export class PlAstParser implements Parser {
             }
             if (variable.content.startsWith(CREATE_MAGIC)) {
                 variable.content = variable.content.slice(1);
-                return new ASTCreate( [ peekToken ], pre, variable, value );
+                return new ASTAssign( [ peekToken ], pre, variable, value );
             }
-            return new ASTAssign( [ peekToken ], pre, variable, value );
+            return new ASTCreate( [ peekToken ], pre, variable, value );
         }
 
         return left;
